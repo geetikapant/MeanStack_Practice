10f9b0840216dcdd3ab656df837cb96c88fc6161
geetikapant <geetikapant0805@gmail.com>
2021-05-25 13:01:34 +0530

    router created

--- Backend/routes/listing.js
+++ Backend/routes/listing.js
@@ -1,7 +1,32 @@
 const router = require("express").Router();
 
+//add new listings
+router.post("/",(req,res)=>{
+    res.send("add new  listings")
+});
+
+
+//get all listings
 router.get("/",(req,res)=>{
     res.send("All listings")
-})
+});
+
+//single listing
+
+router.get("/:listingId",(req,res)=>{
+    res.send("single listing")
+});
+
+//Update listing
+
+router.put("/:listingId",(req,res)=>{
+    res.send("Update listing")
+});
+
+//delete listing
+
+router.delete("/:listingId",(req,res)=>{
+    res.send("delete listing")
+});
 
 module.exports=router;
